edc35d1b5888a8a03cb38ad0f8198a18aae653be
Daniel Vazquez-Silva <danvazquezsilva@gmail.com>
2025-12-30 14:04:37 -0500

    added more functionality

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
+from stats import get_num_words, count_letters, sort_on
+
+
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    count_of_letters = count_letters(text)
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    for key, value in count_of_letters.items():
-        print(f"The '{key}' character was found {value} times")
-    print("--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    listed_dicts = []
+    num_of_words = get_num_words(get_book_text(book_path))
+    letters_dict = count_letters(get_book_text(book_path))
+    for l, count in letters_dict.items():
+        listed_dicts.append({"letter": l, "count": count})
+    listed_dicts.sort(key=sort_on, reverse=True)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_of_words} total words")
+    print("--------- Character Count -------")
+    for i in listed_dicts:
+        print(f"{i['letter']}: {i['count']}")
+    print("============= END ===============")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def count_letters(text):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    text = text.lower()
-    counts = {}
-    for letter in alphabet:
-        counts[letter] = text.count(letter)
-    return counts
 
 
 main()
